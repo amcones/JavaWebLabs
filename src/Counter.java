36eaaea0cc7c3ccadfd5139dbe0f8ad694c1e6ba
jamesamcones <a587212@126.com>
2022-11-18 11:23:39 +0800

    complete lab2

---
+++ src/Counter.java
@@ -0,0 +1,23 @@
+public class Counter {
+    private int count; //计数值
+
+    public Counter() {
+        this(0);
+    }
+
+    public Counter(int count) {
+        this.count = count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    public void add() {
+        count++;
+    }
+}
