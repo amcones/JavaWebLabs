4943757b992795a0bb51c8c5f47fcc9e3de5b19a
jamesamcones <a587212@126.com>
2022-11-25 10:20:18 +0800

    complete lab3 test1

---
+++ src/CounterListener.java
@@ -0,0 +1,39 @@
+import jakarta.servlet.ServletContext;
+import jakarta.servlet.ServletContextEvent;
+import jakarta.servlet.ServletContextListener;
+
+import java.io.*;
+import java.util.Properties;
+
+public class CounterListener implements ServletContextListener {
+    ServletContext servletContext;
+    Properties properties;
+    Counter counter;
+    @Override
+    public void contextInitialized(ServletContextEvent httpSessionEvent){
+        try {
+            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("count.properties"));
+            properties = new Properties();
+            properties.load(bufferedInputStream);
+            servletContext = httpSessionEvent.getServletContext();
+            counter=new Counter(Integer.parseInt(properties.getProperty("count")));
+            System.out.println("Counter已初始化:count="+counter.getCount());
+            servletContext.setAttribute("counter",counter);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+    @Override
+    public void contextDestroyed(ServletContextEvent servletContextEvent){
+        try {
+            properties.setProperty("count",String.valueOf(counter.getCount()));
+            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("count.properties"));
+            properties.store(bufferedOutputStream, "Update");
+            bufferedOutputStream.flush();
+            bufferedOutputStream.close();
+        }catch (Exception e){
+            throw new RuntimeException(e);
+        }
+        System.out.println("count已保存:count="+properties.getProperty("count"));
+    }
+}
--- src/CounterServlet.java
+++ src/CounterServlet.java
@@ -1,53 +1,26 @@
-import jakarta.servlet.RequestDispatcher;
-import jakarta.servlet.ServletConfig;
-import jakarta.servlet.ServletContext;
-import jakarta.servlet.ServletException;
+import jakarta.servlet.*;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.*;
-import java.util.Enumeration;
 import java.util.Properties;
 
 @WebServlet("/CounterServlet")
+
 public class CounterServlet extends HttpServlet {
     ServletContext servletContext;
-    Properties properties;
-    public void init(){
-        try {
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(new FileInputStream("count.properties"));
-            properties = new Properties();
-            properties.load(bufferedInputStream);
-            servletContext = getServletContext();
-            Counter counter=new Counter(Integer.parseInt(properties.getProperty("count")));
-            System.out.println("Counter已初始化:count="+counter.getCount());
-            servletContext.setAttribute("counter",counter);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-    }
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        ServletContext servletContext = getServletContext();
+        servletContext=getServletContext();
         Counter counter = (Counter) servletContext.getAttribute("counter");
         counter.add();
-        properties.setProperty("count",String.valueOf(counter.getCount()));
-        System.out.println("收到请求，count增加，count="+properties.getProperty("count"));
+        request.setAttribute("count",String.valueOf(counter.getCount()));
+        RequestDispatcher imageServlet = request.getRequestDispatcher("image");
+        imageServlet.forward(request,response);
     }
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         doGet(request,response);
     }
-
-    public void destroy(){
-        try {
-            BufferedOutputStream bufferedOutputStream = new BufferedOutputStream(new FileOutputStream("count.properties"));
-            properties.store(bufferedOutputStream, "Update");
-            bufferedOutputStream.flush();
-            bufferedOutputStream.close();
-        }catch (Exception e){
-            throw new RuntimeException(e);
-        }
-        System.out.println("count已保存:count="+properties.getProperty("count"));
-    }
 }
+
